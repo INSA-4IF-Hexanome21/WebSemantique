795b4d303d897292b104bc80a28ad5615f9bac93
Peter Yaacoub <yaapete.dev@gmail.com>
2026-01-06 11:33:03 +0100

    chore: améliorer le code

--- backend/serveur.py
+++ backend/serveur.py
@@ -7,14 +7,13 @@ app = FastAPI()
 # Redirect root to frontend server
 @app.get("/")
 async def root():
-    frontend_ip = os.getenv("FRONTEND_IP", "127.0.0.1")
-    frontend_port = os.getenv("FRONTEND_PORT", "8080")
-    return RedirectResponse(url=f"http://{frontend_ip}:{frontend_port}")
+    frontend_ip = os.getenv("FRONTEND_IP", "0.0.0.0")
+    frontend_port = os.getenv("FRONTEND_PORT", "0")
+    if frontend_port != "0" and frontend_ip != "0.0.0.0":
+        return RedirectResponse(url=f"http://{frontend_ip}:{frontend_port}")
+    else:
+        return { "status": 0, "input": {}, "output": {} }
 
 @app.get("/api/get-planet")
 async def get_planet(name: str):
-    return {
-        "status": "success",
-        "input": {"name": name},
-        "output": {}
-        }
+    return { "status": 1, "input": {"name": name}, "output": {} }
