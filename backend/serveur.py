a73de0b585ac11f5a4027f10e77909ba3d0d762a
Peter Yaacoub <yaapete.dev@gmail.com>
2026-01-13 09:01:08 +0100

    feat: get-constellations

--- backend/serveur.py
+++ backend/serveur.py
@@ -1,8 +1,19 @@
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
+from SPARQLWrapper import SPARQLWrapper, JSON
 
 app = FastAPI()
+sparql = SPARQLWrapper("https://dbpedia.org/sparql")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 # Redirect root to frontend server
 @app.get("/")
@@ -13,6 +24,24 @@ async def root():
         return RedirectResponse(url=f"http://{frontend_ip}:{frontend_port}")
     else:
         return {"status": 0, "input": {}, "output": {}}
+
+# Get constellation names
+@app.get("/api/get-constellations")
+async def get_constellations():
+    query = """
+    SELECT DISTINCT ?nameConstellation
+    WHERE {
+    ?etoile a dbo:Star.
+    ?etoile dbp:constell ?constellation.
+    ?etoile rdfs:label ?label.
+    ?constellation dbp:name ?nameConstellation.
+    FILTER (lang(?label) = "fr")
+    }
+    """
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    return {"status": 1, "input": {}, "output": {results}}
 
 @app.get("/api/get-planet")
 async def get_planet(name: str):
