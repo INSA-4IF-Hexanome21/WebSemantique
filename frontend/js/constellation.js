2ffbc3ca512c23c9520676e61361c99b3a6581aa
elie.ravoux <elie.ravoux@gmail.com>
2026-01-13 11:34:15 +0100

    feat : Mise en forme réponse textuel V1

--- frontend/js/constellation.js
+++ frontend/js/constellation.js
@@ -22,9 +22,20 @@ async function getStarsInConstellation() {
     const response = await fetch("http://127.0.0.1:8000/api/get-stars-in-constellation?name=" + nomConstellation);
     const json = await response.json();
     var list = json["output"];
-    var resultArea = document.getElementById("divReponse");
+    var resultArea = document.getElementById("divReponseTextuelle");
     var textarea = document.createElement("textarea");
+    if (list.length === 0) {
+        list.push("Aucune étoile trouvée dans cette constellation.");
+    }
+    else{
+        list.unshift("");
+        list.unshift("Étoiles dans la constellation " + nomConstellation + " :");
+        list.push("");
+        list.push("Nombre total d'étoiles : " + (list.length - 1).toString());
+    }
     textarea.textContent = list.join("\n");
+    textarea.style.width = "100%";
+    textarea.style.height = "100%";
     resultArea.replaceChildren();
     resultArea.appendChild(textarea);
     resultArea.hidden = false;
